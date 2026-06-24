ee6b687eb276688d8cb42be3cf945c2aea933c8b
João Victor Rocha <111293207+rokfang2@users.noreply.github.com>
2026-06-24 19:42:34 -0300

    Add Door class with state management and observers

--- State.py
+++ State.py
@@ -16,3 +16,19 @@ class DoorState(ABC):
   @abstractmethod
   def status(self, door):
     pass
+
+class Door:
+  def __init__(self):
+    self._state = DoorClosed()
+    self._observers = []
+
+  def set_state(self, state: DoorState):
+    self._state = state
+    self.notify_observers()
+  
+  def add_observer(self, callback):
+    self._observers.append(callback)
+
+  def notify_observers(self):
+    for observer in self._observers:
+      observer(self.status())
