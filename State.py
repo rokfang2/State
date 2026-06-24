3a20ce817fb8a288c2ccdef1425d4b5bdf562750
João Victor Rocha <111293207+rokfang2@users.noreply.github.com>
2026-06-24 19:10:18 -0300

    Create DoorState
    Added abstract base class for door states with required methods.

--- State.py
+++ State.py
@@ -1,1 +1,18 @@
+from abc import ABC, abstractmethod
 
+class DoorState(ABC):
+  @abstractmethod
+  def click(self, door):
+    pass
+
+  @abstractmethod
+  def complete(self, door):
+    pass
+
+  @abstractmethod
+  def timeout(self, door):
+    pass
+
+  @abstractmethod
+  def status(self, door):
+    pass
